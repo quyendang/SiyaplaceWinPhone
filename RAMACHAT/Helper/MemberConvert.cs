917090ece15d69ea0a6f3e592e5725e21b1cbeb9
quyendang <Cucainho214>
2015-08-10 11:02:24 +0700

    background task

---
+++ RAMACHAT/Helper/MemberConvert.cs
@@ -0,0 +1,28 @@
+﻿using RAMACHAT.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RAMACHAT.Helper
+{
+    public class MemberConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string mem = "";
+            foreach (var item in (System.Collections.Generic.List<Member>)value)
+            {
+                mem = mem + item.username;
+            }
+            return mem;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
